5c01b6954d7be5012d0341bf5a91760eb4c46bf0
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2020-07-17 08:01:44 +0200

    update progress bar

--- Assets/PcSoft/ExtendedUI/90 Scripts/Components/UI/Component/UiProgressBar.cs
+++ Assets/PcSoft/ExtendedUI/90 Scripts/Components/UI/Component/UiProgressBar.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -9,16 +10,19 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Component
     public sealed class UiProgressBar : UIBehaviour
     {
         #region Inspector Data
-        
+
         [Header("Values")]
         [SerializeField]
         [Range(0f, 1f)]
         private float value = 0.5f;
 
+        [SerializeField]
+        private ProgressFillType fillType = ProgressFillType.Image;
+
         [Header("References")]
         [SerializeField]
         private Image progressImage;
-        
+
         [SerializeField]
         private Image fillPointImage;
 
@@ -27,12 +31,36 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Component
 
         #endregion
 
+        #region Properties
+
         public float Value
         {
-            get => progressImage.fillAmount;
+            get
+            {
+                switch (fillType)
+                {
+                    case ProgressFillType.Image:
+                        return progressImage.fillAmount;
+                    case ProgressFillType.Slider:
+                        return _slider.value;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
             set
             {
-                progressImage.fillAmount = value;
+                switch (fillType)
+                {
+                    case ProgressFillType.Image:
+                        progressImage.fillAmount = value;
+                        break;
+                    case ProgressFillType.Slider:
+                        _slider.value = value;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+
                 if (fillPointImage != null)
                 {
                     var ownRectTrans = (RectTransform) transform;
@@ -42,20 +70,57 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Component
             }
         }
 
+        #endregion
+
+        private Slider _slider;
+
         #region Builtin Methods
-        
+
         protected override void Awake()
         {
+            _slider = GetComponent<Slider>();
             Value = value;
         }
 
 #if UNITY_EDITOR
         protected override void OnValidate()
         {
-            Value = value;
-        }  
+            switch (fillType)
+            {
+                case ProgressFillType.Image:
+                    if (gameObject.GetComponent<Slider>() != null)
+                    {
+                        var cmp = gameObject.GetComponent<Slider>();
+                        cmp.value = 1f;
+
+                        DestroyImmediate(cmp);
+                        _slider = null;
+                    }
+
+                    break;
+                case ProgressFillType.Slider:
+                    if (gameObject.GetComponent<Slider>() == null)
+                    {
+                        _slider = gameObject.AddComponent<Slider>();
+                        _slider.interactable = false;
+                        _slider.fillRect = progressImage.rectTransform;
+                    }
+
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            Awake();
+        }
 #endif
 
         #endregion
+    }
+
+    public enum ProgressFillType
+    {
+        Image,
+        Slider
     }
 }
