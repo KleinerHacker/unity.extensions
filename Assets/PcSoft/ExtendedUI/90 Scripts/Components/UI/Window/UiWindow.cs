0c6a1124cba2f134cb71a97f2cd150804e1c1182
Kleiner Hacker <Christoph.Pfeiffer.Berlin@freenet.de>
2020-09-08 20:46:59 +0200

    insert UI support for new input system for window

--- Assets/PcSoft/ExtendedUI/90 Scripts/Components/UI/Window/UiWindow.cs
+++ Assets/PcSoft/ExtendedUI/90 Scripts/Components/UI/Window/UiWindow.cs
@@ -1,12 +1,10 @@
 using System;
-using PcSoft.ExtendedAnimation._90_Scripts.Types;
-using PcSoft.ExtendedAnimation._90_Scripts.Utils;
-using PcSoft.ExtendedUI._90_Scripts.Utils.Extensions;
 using PcSoft.ExtendedUnity._90_Scripts._00_Runtime.Utils;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.EventSystems;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Window
 {
@@ -16,23 +14,35 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Window
     public class UiWindow : UiStage
     {
         #region Inspector Data
-        
+
         [SerializeField]
         private DialogEscapeAction escapeAction = DialogEscapeAction.None;
 
         [Header("SFX")]
         [SerializeField]
         private AudioMixerGroup audioMixerGroup;
-        
+
         [SerializeField]
         private AudioClip openClip;
-        
+
         [Header("Game Behavior")]
         [SerializeField]
         private bool blockingGame = false;
 
         [SerializeField]
         private bool changeCursorSystem = false;
+
+        [Header("Input System")]
+#if ENABLE_INPUT_SYSTEM
+        [SerializeField]
+        private InputActionAsset inputAction;
+
+        [SerializeField]
+        private string actionId;
+#else
+        [SerializeField]
+        private string action = "Cancel";
+#endif
 
         #endregion
 
@@ -43,49 +53,104 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Window
         #endregion
 
         private AudioSource _audioSource;
+#if ENABLE_INPUT_SYSTEM
+        private InputAction _ia;
+#endif
 
         #region Builtin Methods
 
         protected override void Awake()
         {
             base.Awake();
-            
+
             _audioSource = gameObject.AddComponent<AudioSource>();
             _audioSource.playOnAwake = false;
             _audioSource.loop = false;
             _audioSource.outputAudioMixerGroup = audioMixerGroup;
+
+#if ENABLE_INPUT_SYSTEM
+            _ia = inputAction.FindAction(actionId, true);
+#endif
         }
 
+        protected override void OnEnable()
+        {
+#if ENABLE_INPUT_SYSTEM
+            _ia.performed += PlayerInputOnActionTriggered;
+            _ia.Enable();
+#endif
+        }
+
+        protected override void OnDisable()
+        {
+#if ENABLE_INPUT_SYSTEM
+            _ia.Disable();
+            _ia.performed -= PlayerInputOnActionTriggered;
+#endif
+        }
+
+#if !ENABLE_INPUT_SYSTEM
         private void Update()
         {
-            if (Input.GetButtonUp("Cancel"))
+            if (Input.GetButtonUp(action))
             {
-                switch (EscapeAction)
-                {
-                    case DialogEscapeAction.None:
-                        break;
-                    case DialogEscapeAction.Toggle:
-                        Debug.Log("Toggle dialog (escape)", this);
-                        if (State == ViewableState.Shown)
-                            Hide();
-                        else 
-                            Show();
-                        break;
-                    case DialogEscapeAction.HideOnly:
-                        if (State == ViewableState.Shown)
-                        {
-                            Debug.Log("Hide dialog (escape)", this);
-                            Hide();
-                        }
+                HandleToggle();
+            }
+        }
+#endif
 
-                        break;
-                    default:
-                        throw new NotImplementedException();
-                }
+#if ENABLE_INPUT_SYSTEM && UNITY_EDITOR
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+
+            if (inputAction == null)
+                return;
+
+            var enumerator = inputAction.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                actionId = current.id.ToString();
+            }
+        }
+#endif
+
+        #endregion
+
+        private void HandleToggle()
+        {
+            switch (EscapeAction)
+            {
+                case DialogEscapeAction.None:
+                    break;
+                case DialogEscapeAction.Toggle:
+                    Debug.Log("Toggle dialog (escape)", this);
+                    if (State == ViewableState.Shown)
+                        Hide();
+                    else
+                        Show();
+                    break;
+                case DialogEscapeAction.HideOnly:
+                    if (State == ViewableState.Shown)
+                    {
+                        Debug.Log("Hide dialog (escape)", this);
+                        Hide();
+                    }
+
+                    break;
+                default:
+                    throw new NotImplementedException();
             }
         }
 
-        #endregion
+#if ENABLE_INPUT_SYSTEM
+        private void PlayerInputOnActionTriggered(InputAction.CallbackContext obj)
+        {
+            Debug.Log("*** ESC ***");
+            HandleToggle();
+        }
+#endif
 
         protected override void OnShowing()
         {
@@ -93,7 +158,7 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Window
             {
                 _audioSource.PlayOneShot(openClip);
             }
-            
+
             if (blockingGame)
             {
                 GameTimeController.Pause(this, fadingCurve, fadingSpeed);
@@ -116,7 +181,7 @@ namespace PcSoft.ExtendedUI._90_Scripts.Components.UI.Window
                 Cursor.visible = false;
                 Cursor.lockState = CursorLockMode.Locked;
             }
-            
+
             if (blockingGame)
             {
                 GameTimeController.Resume(this, fadingCurve, fadingSpeed);
